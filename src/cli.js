1014b28df34a0e2f2db5e9303ec4607914299aca
Gayan Kuruppu <gayan.kuruppu@live.com>
2021-05-23 15:32:10 +0530

    Update Action

--- src/cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-// const index = require('./index');
-const cli = async () => {
-    console.log("simple run")
-};
-cli().then(() => {});
--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ const git = require('./git/git');
 const JsonFile = require('./model/JsonFile');
 let Index = function () {
     let main = async function () {
-        const DIRECTORY = 'directory';
-        const FILE = 'file';
-        const PATH = `${DIRECTORY}/${FILE}.json`;
-        const JSON_OBJECT = new JsonFile(new Date());
-        const USERNAME = 'Upptime Bot';
-        const EMAIL = '73812536+upptime-bot@users.noreply.github.com';
-        const BRANCH = 'main';
-        const MESSAGE = 'Update App';
-        await git.pull();
-        await directory.createDirectory(DIRECTORY);
-        await directory.createGitIgnore(DIRECTORY);
-        let readJson = await file.readJson(PATH);
-        core.info(JSON.stringify(readJson));
-        await file.createJson(PATH, JSON_OBJECT);
-        let postJson = await file.readJson(PATH);
-        core.info(JSON.stringify(postJson));
-        await git.commit(USERNAME, EMAIL, BRANCH, MESSAGE);
-        await git.push(BRANCH);
+        console.log("simple run")
+        // const DIRECTORY = 'directory';
+        // const FILE = 'file';
+        // const PATH = `${DIRECTORY}/${FILE}.json`;
+        // const JSON_OBJECT = new JsonFile(new Date());
+        // const USERNAME = 'Upptime Bot';
+        // const EMAIL = '73812536+upptime-bot@users.noreply.github.com';
+        // const BRANCH = 'main';
+        // const MESSAGE = 'Update App';
+        // await git.pull();
+        // await directory.createDirectory(DIRECTORY);
+        // await directory.createGitIgnore(DIRECTORY);
+        // let readJson = await file.readJson(PATH);
+        // core.info(JSON.stringify(readJson));
+        // await file.createJson(PATH, JSON_OBJECT);
+        // let postJson = await file.readJson(PATH);
+        // core.info(JSON.stringify(postJson));
+        // await git.commit(USERNAME, EMAIL, BRANCH, MESSAGE);
+        // await git.push(BRANCH);
         
     }
     return {
-        main: main,
+        run: main,
     };
 }();
-module.exports = Index;
+Index.run().then(() => {});
