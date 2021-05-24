28fa859a4fc22de75a2e32254583beb364ba0604
Gayan Kuruppu <gayan.kuruppu@live.com>
2021-05-24 17:28:04 +0530

    Update Action

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const core = require('@actions/core');
 const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
-const directory = require('./file/directory');
 const file = require('./file/file');
-const git = require('./git/git');
-const JsonFile = require('./model/JsonFile');
 let Index = function () {
     let main = async function (commandModel) {
         const chartJSNodeCanvas = new ChartJSNodeCanvas({ width: 600, height: 400 });
@@ -11,12 +8,6 @@ let Index = function () {
         const configLabels = JSON.parse(commandModel.labels);
         const configDataset = JSON.parse(commandModel.dataset);
         const configOutputFile= commandModel.outputFile;
-
-        const USERNAME = 'Upptime Bot';
-        const EMAIL = '73812536+upptime-bot@users.noreply.github.com';
-        const BRANCH = 'main';
-        const MESSAGE = 'Update App';
-
         core.info(configType + ' ' + configLabels + ' ' + configDataset + ' ' + configOutputFile)
         core.info(JSON.stringify(configDataset));
         let datasets = [];
@@ -32,10 +23,7 @@ let Index = function () {
             }
         };
         const image = await chartJSNodeCanvas.renderToBuffer(configuration);
-        await git.pull();
         await file.createImage(configOutputFile, image);
-        await git.commit(USERNAME, EMAIL, BRANCH, MESSAGE);
-        await git.push(BRANCH);
     }
     return {
         main: main,
