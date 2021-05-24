047ea4e038e6dba9d95f12ad46d5baec87e2a28e
Gayan Kuruppu <gayan.kuruppu@live.com>
2021-05-24 09:55:47 +0530

    Update Action

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const index = require('./index');
+// const index = require('./index');
 const cli = async () => {
-    console.log(`out put ${process.argv[1]}`)
-    return index.main();
+    console.log(`out put ${process.argv[2]}`)
+    // return index.main();
 };
 cli().then(() => {});
